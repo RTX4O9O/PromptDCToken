60786de416112b47c341045a70d4ac44aef7dd36
RTX <jaykuo1203@gmail.com>
2024-06-09 14:06:47 +0800

    update build setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,12 @@
 plugins {
     id("java")
+    application
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
+application {
+    mainClass.set("me.rtx4090.Main")
+}
+
 
 group = "me.rtx4090"
 version = "1.0-SNAPSHOT"
@@ -14,9 +20,25 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("org.seleniumhq.selenium:selenium-java:4.21.0")
 
-
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "com.example.MainClass"
+    }
+    from({
+        configurations.runtimeClasspath.get().map { if (it.isDirectory) it else zipTree(it) }
+    })
+}
+
+tasks {
+    shadowJar {
+        archiveBaseName.set("TokenLogin")
+        archiveVersion.set("1.0-SNAPSHOT")
+        archiveClassifier.set("")
+    }
 }
